0cbc09f6d6e9fc7af9d43720ba3943b8d3ff6c82
Sulaymon Yahyo 🪐 <sulaymon1w@gmail.com>
2023-07-26 13:49:51 +0500

    🧱 First comment

---
+++ src/main/java/org/suyo/springboottelegram/SpringbootTelegramApplication.java
@@ -0,0 +1,13 @@
+package org.suyo.springboottelegram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTelegramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootTelegramApplication.class, args);
+    }
+
+}
